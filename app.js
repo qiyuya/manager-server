d6f039f82fac5c9bd488d66696033300cba77737
qiyu <157453032@qq.com>
2022-03-05 20:14:35 +0800

    部门列表及交互后台实现

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ const util = require('./utils/util')
 const users = require('./routes/users')
 const menus = require('./routes/menus')
 const roles = require('./routes/roles')
+const depts = require('./routes/depts')
 
 // error handler
 onerror(app)
@@ -56,6 +57,7 @@ app.use(koajwt({secret:'qiyu'}).unless({
 router.use(users.routes(), users.allowedMethods())
 router.use(menus.routes(), menus.allowedMethods())
 router.use(roles.routes(), roles.allowedMethods())
+router.use(depts.routes(), depts.allowedMethods())
  
 app.use(router.routes(), router.allowedMethods())
 
